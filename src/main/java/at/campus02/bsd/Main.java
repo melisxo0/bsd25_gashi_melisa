fae3b950b3754da8974195d621d07602cc7cf3dc
Melisa Gashi <52505183@edu-campus02.at>
2026-06-17 00:42:47 +0200

    add calculator class and main class

---
+++ src/main/java/at/campus02/bsd/Main.java
@@ -0,0 +1,16 @@
+package at.campus02.bsd;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+
+        System.out.println("Add: " + calc.add(10, 5));
+        System.out.println("Subtract: " + calc.subtract(10, 5));
+        System.out.println("Divide: " + calc.divide(10, 5));
+        System.out.println("Multiply: " + calc.multiply(10, 5));
+
+        System.out.println("Melisa Gashi");
+
+    }
+}
